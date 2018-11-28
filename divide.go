943da7e8bde42e15b6b78ebae96de106443b92a7
ben.wang <wangben04@meituan.com>
2018-11-28 20:23:50 +0800

    Initial commit

---
+++ divide.go
@@ -0,0 +1,16 @@
+
+package main
+import "fmt"
+/***
+Given two integers dividend and divisor, divide two integers without using multiplication, division and mod operator.
+Return the quotient after dividing dividend by divisor.
+The integer division should truncate toward zero.
+***/
+func divide(dividend int, divisor int) int {
+
+	return -1;
+}
+
+func main() {
+	fmt.Println("hello world")
+}
